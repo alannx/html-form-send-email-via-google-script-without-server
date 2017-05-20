a5e3bc6c4c36d7fca337e273b027f37c6bdd28cf
alannx <adamtwice98@gmail.com>
2017-05-21 03:51:58 +0800

    Update and rename form-submission-handler.js to form-submission-handlerr.js

--- form-submission-handlerr.js
+++ form-submission-handlerr.js
@@ -5,7 +5,7 @@ function validEmail(email) { // see:
 }
 // get all data in form and return object
 function getFormData() {
-  var elements = document.getElementById("gform").elements; // all form elements
+  var elements = document.getElementById("cform").elements; // all form elements
   var fields = Object.keys(elements).map(function(k) {
     if(elements[k].name !== undefined) {
       return elements[k].name;
@@ -57,7 +57,7 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
     xhr.onreadystatechange = function() {
         console.log( xhr.status, xhr.statusText )
         console.log(xhr.responseText);
-        document.getElementById('gform').style.display = 'none'; // hide form
+        document.getElementById('cform').style.display = 'none'; // hide form
         document.getElementById('thankyou_message').style.display = 'block';
         return;
     };
@@ -71,7 +71,7 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
 function loaded() {
   console.log('contact form submission handler loaded successfully');
   // bind to the submit event of our form
-  var form = document.getElementById('gform');
+  var form = document.getElementById('cform');
   form.addEventListener("submit", handleFormSubmit, false);
 };
 document.addEventListener('DOMContentLoaded', loaded, false);
